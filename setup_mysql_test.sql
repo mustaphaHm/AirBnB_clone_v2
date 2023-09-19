fb986fedcbb3a515fac33b40a294e328673345bc
MustaphaHm <mustapha.houmam@gmail.com>
2023-09-19 07:56:57 -0700

    Task 3, script that prepares a MySQL server for the projec

---
+++ setup_mysql_test.sql
@@ -0,0 +1,14 @@
+-- Create Database
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create User
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges to the user on all database
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
