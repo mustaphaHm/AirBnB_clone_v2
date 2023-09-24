76fdb6a6401690ca3f74af6bdce4fe5f07793f40
MustaphaHm <mustapha.houmam@gmail.com>
2023-09-23 18:01:03 -0700

    test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,14 +1,14 @@
 -- Create Database
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db
 
 -- Create User
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
 
--- Grant all privileges to the user on all database
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Grant all privileges to the user on all databases
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost'
 
 -- Grant SELECT privilege on performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
 
 -- Flush privileges to apply changes
-FLUSH PRIVILEGES;
+FLUSH PRIVILEGES
